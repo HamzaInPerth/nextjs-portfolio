719d048f1bd8809ca3661466450b4abbbf231a19
Hamza <chikhi.h@gmail.com>
2023-12-10 23:34:57 -0500

    feat: clickoutside on menu

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
@@ -22,6 +22,29 @@ const navLinks = [
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarRef = useRef(null);
+
+  const handlerNavbarState = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const handleOutsideClick = (event) => {
+    if (navbarOpen && navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setNavbarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (navbarOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [navbarOpen]);
 
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
@@ -33,21 +56,14 @@ const Navbar = () => {
           LOGO
         </Link>
         <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
+
             <button
-              onClick={() => setNavbarOpen(true)}
+              onClick={() => setNavbarOpen(!navbarOpen)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              <Bars3Icon className="h-5 w-5" />
+              {navbarOpen ? (<XMarkIcon className="h-5 w-5" />) : (<Bars3Icon className="h-5 w-5" />)}
+              
             </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
@@ -59,7 +75,9 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      <ul ref={navbarRef} className="flex flex-col py-4 items-center">
+        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      </ul>
     </nav>
   );
 };
